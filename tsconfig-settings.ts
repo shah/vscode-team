38f3a690b35d51d47742f2f91a5c410fdcc63839
Shahid N. Shah <shahid@shah.org>
2020-10-30 20:38:41 -0400

    Add default tsconfig content

---
+++ tsconfig-settings.ts
@@ -0,0 +1,71 @@
+export interface TypeScriptCompilerConfig {
+  compilerOptions: CompilerOptions;
+  include: string[];
+  exclude: string[];
+}
+
+export interface CompilerOptions {
+  outDir: string;
+  declaration: boolean;
+  sourceMap: boolean;
+  target: string;
+  lib: string[];
+  allowJs: boolean;
+  skipLibCheck: boolean;
+  esModuleInterop: boolean;
+  allowSyntheticDefaultImports: boolean;
+  strict: boolean;
+  alwaysStrict: boolean;
+  noImplicitAny: boolean;
+  forceConsistentCasingInFileNames: boolean;
+  module: string;
+  moduleResolution: string;
+  resolveJsonModule: boolean;
+  isolatedModules: boolean;
+  noEmit: boolean;
+  jsx: string;
+  typeRoots: string[];
+  emitDecoratorMetadata: boolean;
+  experimentalDecorators: boolean;
+}
+
+export const defaultTsConfig: TypeScriptCompilerConfig = {
+  compilerOptions: {
+    outDir: "dist",
+    declaration: true,
+    sourceMap: true,
+    target: "es6",
+    lib: [
+      "dom",
+      "dom.iterable",
+      "esnext",
+    ],
+    allowJs: true,
+    skipLibCheck: true,
+    esModuleInterop: true,
+    allowSyntheticDefaultImports: true,
+    strict: true,
+    alwaysStrict: true,
+    noImplicitAny: true,
+    forceConsistentCasingInFileNames: true,
+    module: "umd",
+    moduleResolution: "node",
+    resolveJsonModule: false,
+    isolatedModules: true,
+    noEmit: false,
+    jsx: "preserve",
+    typeRoots: [
+      "./node_modules/@types",
+    ],
+    emitDecoratorMetadata: true,
+    experimentalDecorators: true,
+  },
+  include: [
+    "**/*.ts",
+  ],
+  exclude: [
+    "**/node_modules",
+    "dist",
+    "**/.*/",
+  ],
+};
