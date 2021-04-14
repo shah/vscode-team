9a1e5f67acdccd5c439e632b713cdbd7134b5a49
Shahid N. Shah <shahid@shah.org>
2021-04-14 17:32:19 -0400

    fix: use temporary docopt.js for Deno 1.9 upgrade

--- deps.ts
+++ deps.ts
@@ -1,6 +1,8 @@
 export * as fs from "https://deno.land/std@0.87.0/fs/mod.ts";
 export * as path from "https://deno.land/std@0.87.0/path/mod.ts";
-export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.6/src/docopt.ts";
+//export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.6/src/docopt.ts";
+// TODO: when Eyal-Shalev/docopt.js is upgraded, replace this with latest version:
+export * as docopt from "https://denopkg.com/netspective-studios/docopt-TEMP.js/src/docopt.ts";
 export * as govnSvcVersion from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.15/version.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.1/mod.ts";
 export * as tsdsh from "https://denopkg.com/shah/tsd-shell@v0.9.2/mod.ts";
